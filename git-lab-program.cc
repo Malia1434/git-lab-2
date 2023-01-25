e7565cdd993b566ebdc7decab6d7342c49f25b5d
Malia1434 <md336821@ohio.edu>
2023-01-24 19:52:06 -0500

    Good

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Malia Dorsey>
+ *        Date: <1/24/2023>
+ * Description: Using Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout<< "Hello Git!! I'm tired" << enum;
+    return 0;
+}// main
